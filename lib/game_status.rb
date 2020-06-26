076b6361c9e1730f278ec16b70971785a6771312
HAN O <a.ocal88@gmail.com>
2020-06-26 21:33:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,45 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], #top_row
+  [3,4,5], #middle_row
+  [6,7,8], #bottom_row
+  [0,3,6], #left colomn
+  [1,4,7], #center colomn
+  [2,5,8], #right colomn
+  [0,4,8], #left_diagonal
+  [2,4,6] #right_diagonal
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  win_index_4 = win_combination[3]
+  win_index_5 = win_combination[4]
+  win_index_6 = win_combination[5]
+  win_index_7 = win_combination[6]
+  win_index_8 = win_combination[7]
+
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+  position_4 = board[win_index_4]
+  position_5 = board[win_index_5]
+  position_6 = board[win_index_6]
+  position_7 = board[win_index_7]
+  position_8 = board[win_index_8]
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination
+  else
+    false
+  end
+end
+end
